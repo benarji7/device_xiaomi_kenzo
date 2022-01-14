adcff41199561d4c3ba70a95f5d627d79dd43914
Benarji Anand <benarji385@gmail.com>
2022-01-14 09:31:25 +0800

    kenzo: Update fingerprint build

--- lineage_kenzo.mk
+++ lineage_kenzo.mk
@@ -31,7 +31,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.2.1.0.MHOCNDL release-keys"
+    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V10.2.1.0.MHOMIXM release-keys"
 
-BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys
+BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V10.2.1.0.MHOMIXM:user/release-keys
 
